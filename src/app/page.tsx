636bea822f8722548cb13a917b921b2e07361672
Malessan4 <matialess@gmail.com>
2025-07-26 05:35:19 -0300

    se crea el componente particles y se instala tsparticles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-import Image from "next/image";
+import CoverParticles from "./components/cover-particles"
+
 
 export default function Home() {
   return (
-     <p>From Home</p>
+    <main>
+      <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover">
+        <CoverParticles />
+        <p></p>
+      </div>
+    </main>
+
   );
 }
